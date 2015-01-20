85ef8216e4a01c0a990c16dfbf53ae147a541536
V3rnon <black.tree@yahoo.com>
2015-01-20 09:08:57 +0200

    Create Myscript.sh

---
+++ Myscript.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "This is my script. It will show you the jpg file size sum.";
+tree -P "*.jpg"  Desktop/Bash;
+du -h Desktop/Bash/*.jpg  | awk '{ print; total += $1 }; END { print "Total size of jpg files: ",total }';
+
